a6637d41aad8489f9f5e5d7ca9a89ea3168ba401
Samuel Toloza <tlz.system@gmail.com>
2026-09-23 01:41:57 -0300

    fix to use collectstatic from django

--- app/settings/base.py
+++ app/settings/base.py
@@ -31,8 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'tournament',
 ]
